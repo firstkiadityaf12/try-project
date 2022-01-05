7247bba516bd18860c19527398a9aa96fd538f7f
aditya <adityafernanda2013@gmail.com>
2022-01-05 18:33:37 +0700

    backend recreate

---
+++ models/member.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.transaksi, {
+        foreignKey: "id_transaksi",
+        as: "transaksi"
+      })
+    }
+  };
+  member.init({
+    id_member: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nama: DataTypes.STRING,
+    alamat: DataTypes.TEXT,
+    jenis_kelamin: DataTypes.ENUM('L','P'),
+    telpon: DataTypes.INTEGER
+  }, {
+      sequelize,
+      modelName: 'member',
+      tableName: 'member'
+    });
+  return member;
+};
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+
+app.listen(8000, () => {
+    console.log("Server run on port 8000");
+})
